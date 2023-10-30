182b011acaed8e722aaca17acf6fcc61b8fd55fc
AlfengYuan <2640706467@qq.com>
2023-10-30 11:30:41 +0800

    add mmpretrain/myself_tools

---
+++ mmpretrain/myself_tools/export_model.py
@@ -0,0 +1,31 @@
+import torch
+import torch.onnx
+from torchvision.models import resnet50
+from mmpretrain import get_model
+
+# from collections import OrderedDict
+#
+# mmlab_model = torch.load("../work_dirs/resnet50_8xb32_in1k/epoch_100.pth", map_location=torch.device('cpu'))
+# print(mmlab_model['state_dict'])
+
+# mmlabkeys = list(mmlab_model['state_dict'].keys())
+# print(mmlabkeys)
+
+
+def torchvision_resnet50_2_onnx():
+    input = torch.randn(1, 3, 224, 224)
+    model = resnet50(weights='ResNet50_Weights.IMAGENET1K_V1')
+    model.eval()
+    torch.onnx.export(model, input, "torchvision_resnet50.onnx")
+
+def get_mmpretrain_resnet50_onnx():
+    input = torch.randn(1, 3, 224, 224)
+    model = get_model("resnet50_8xb32_in1k", pretrained="../work_dirs/resnet50_8xb32_in1k/epoch_100.pth")
+    model.eval()
+    torch.onnx.export(model, input, "mmpretrain_resnet50.onnx")
+
+
+if __name__ == '__main__':
+    # torchvision_resnet50_2_onnx()
+    # get_mmpretrain_resnet50_model()
+    print("return 0")
