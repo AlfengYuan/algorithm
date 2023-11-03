99a7d55f5b019dcfc0978819f14ee995324c6e0a
AlfengYuan <2640706467@qq.com>
2023-11-03 16:23:07 +0800

    sqsy update.

--- deploy_sophon/sophon-app/src/resnet50_sail_main.cpp
+++ deploy_sophon/sophon-app/src/resnet50_sail_main.cpp
@@ -16,7 +16,7 @@ You may obtain a copy of the License at
 
 #include "processor.h"
 #include "json/json.h"
-
+#include "MQTTClient.h"
 
 #if defined(__GNUC__)
 #pragma GCC diagnostic push
@@ -24,6 +24,13 @@ You may obtain a copy of the License at
 #elif defined(__MSC_VER)
 #pragma warning(disable: 4996)
 #endif
+
+#define ADDRESS     "tcp://broker.emqx.io:1883"
+#define CLIENTID    "ExampleClientPub"
+#define TOPIC       "MQTT Examples"
+#define PAYLOAD     "Hello World!"
+#define QOS         2
+#define TIMEOUT     10000L
 
 using namespace std;
 
@@ -44,6 +51,56 @@ public:
     SQSY state;
     string addres;
 }Cameras;
+
+int MQTT_PubMessage(Cameras &camera)
+{
+
+    // creat MQTTClient
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+    char buffer[200];
+
+    int rc = 0;
+    if ((rc = MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to create client, return code %d\n", rc);
+        exit(EXIT_FAILURE);
+    }
+
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(EXIT_FAILURE);
+    }
+
+    sprintf(buffer, "{\"address\":%s, \"state\": %d}", camera.addres.c_str(), int(camera.state));
+    pubmsg.payload = buffer;
+    pubmsg.payloadlen = (int)strlen(buffer);
+    pubmsg.qos = QOS;
+    pubmsg.retained = 0;
+    if ((rc = MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to publish message, return code %d\n", rc);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Waiting for up to %d seconds for publication of %s\n"
+                "on topic %s for client with ClientID: %s\n",
+                (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
+    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+    printf("Message with delivery token %d delivered\n", token);
+
+    if ((rc = MQTTClient_disconnect(client, 10000)) != MQTTCLIENT_SUCCESS)
+        printf("Failed to disconnect, return code %d\n", rc);
+
+    MQTTClient_destroy(&client);
+
+    return 0;
+}
 
 /**
  * @brief Load a bmodel and do inference.
@@ -267,9 +324,7 @@ int main(int argc, char *argv[]){
             if(mycameras[i].state == SQSY::NORMAL) continue;
 
             // MQTTMessage publish
-            cout << "=====================" << endl;
-            cout << "output: " << int(out_state) << endl;
-            cout << "=====================" << endl;
+            MQTT_PubMessage(mycameras[i]);
         }
     }
 
