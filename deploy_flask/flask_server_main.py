9813de520f70c11267b0ebee09adc8ff323a0f6b
AlfengYuan <2640706467@qq.com>
2023-11-15 16:05:00 +0800

    update face-compare

--- deploy_flask/flask_server_main.py
+++ deploy_flask/flask_server_main.py
@@ -18,6 +18,7 @@ app = Flask(__name__)
 projects = {"face": ["cjjb"], "upload": ["sqsy"]}
 
 FACE_ENCODE_URL = '/face-encode/<project>'
+FACE_COMPARE_URL = '/face-compare/<project>'
 UPLOAD_URL = '/upload/<project>'
 
 @app.route(FACE_ENCODE_URL, methods=['POST'])
@@ -39,6 +40,32 @@ def face_encode(project):
             return {"error": "not support {}".format(project)}
     else:
         return {"error": "not right image data, please use json type"}
+
+@app.route(FACE_COMPARE_URL, methods=['POST'])
+def face_compare(project):
+    if request.method != 'POST':
+        return {"error": "request method only support POST"}
+    if request.json.get('image') and request.json.get('features'):
+        im_bytes = base64.b64decode(request.json['image'].encode("utf-8"))
+        # im_bytes = im_file.read()
+        im = np.array(Image.open(io.BytesIO(im_bytes)).convert('RGB'))
+        list_of_face_encodings = list(map(lambda x: np.array(list(map(lambda y: float(y), x.split(',')))),
+                                          request.json.get('features').split(';')))
+        if project in projects["face"]:
+            # pdb.set_trace()
+            a_single_unknow_face_encoding = face_recognition.face_encodings(face_image=im, known_face_locations=None,
+                                                                            num_jitters=1, model='large')
+            if len(a_single_unknow_face_encoding) <= 0:
+                return {"error": "not find face in image"}
+
+            distances_index = str(face_recognition.face_distance(list_of_face_encodings,
+                                                                 a_single_unknow_face_encoding[0]).argmin())
+
+            return distances_index
+        else:
+            return {"error": "not support {}".format(project)}
+    else:
+        return {"error": "not right image data or features data, please use correct json type"}
 
 @app.route("/my_fcn")
 def my_fcn():
