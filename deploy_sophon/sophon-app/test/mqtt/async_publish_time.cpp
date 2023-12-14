f1767014e320b57697a605a8c7f594f97ad9e52e
AlfengYuan <2640706467@qq.com>
2023-12-14 11:32:47 +0800

    update

--- deploy_sophon/sophon-app/test/mqtt/async_publish_time.cpp
+++ deploy_sophon/sophon-app/test/mqtt/async_publish_time.cpp
@@ -54,7 +54,7 @@
 using namespace std;
 using namespace std::chrono;
 
-const std::string DFLT_SERVER_ADDRESS {"tcp://192.168.0.102:1883" };
+const std::string DFLT_SERVER_ADDRESS {"mqtt://localhost:1883" };
 
 // The QoS for sending data
 const int QOS = 1;
