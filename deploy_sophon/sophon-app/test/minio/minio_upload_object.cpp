305904dfadcc2b7ace021a51680addc06eb831e2
AlfengYuan <2640706467@qq.com>
2023-12-18 16:44:30 +0800

    update

--- deploy_sophon/sophon-app/test/minio/minio_upload_object.cpp
+++ deploy_sophon/sophon-app/test/minio/minio_upload_object.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
     minio::s3::BaseUrl base_url(address, false);
     // Create credential provider
     minio::creds::StaticProvider provider(
-            "yyf", "yz123456"
+            "Q3AM3UQ867SPQQA43P2F", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
     );
     // Create S3 client
     minio::s3::Client client(base_url, &provider);
@@ -28,12 +28,11 @@ int main(int argc, char *argv[]) {
     // Check bucket exist or not.
     bool exist;
     {
-        minio::s3::BucketExistsArgs  bktExtArgs;
+        minio::s3::BucketExistsArgs bktExtArgs;
         bktExtArgs.bucket = uploadArgs.bucket;
 
         minio::s3::BucketExistsResponse resp = client.BucketExists(bktExtArgs);
-        if(!resp)
-        {
+        if (!resp) {
             std::cout << "unable to do bucket existence check; " << resp.Error() << std::endl;
             return EXIT_FAILURE;
         }
@@ -42,14 +41,12 @@ int main(int argc, char *argv[]) {
     }
 
     // Make bucket if not exist
-    if(!exist)
-    {
+    if (!exist) {
         minio::s3::MakeBucketArgs makeBktArgs;
         makeBktArgs.bucket = uploadArgs.bucket;
 
         minio::s3::MakeBucketResponse resp = client.MakeBucket(makeBktArgs);
-        if(!resp)
-        {
+        if (!resp) {
             std::cout << "unable to create bucket; " << resp.Error() << std::endl;
             return EXIT_FAILURE;
         }
@@ -57,8 +54,7 @@ int main(int argc, char *argv[]) {
 
     // Call upload object
     minio::s3::UploadObjectResponse resp = client.UploadObject(uploadArgs);
-    if(!resp)
-    {
+    if (!resp) {
         std::cout << "unable to upload object; " << resp.Error() << std::endl;
         return EXIT_FAILURE;
     }
